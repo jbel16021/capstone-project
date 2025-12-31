1115c80a7c1393934550e5dec38a9a02acd99e26
Jose Beltran <beltran.jose.carlos@gmail.com>
2025-12-31 15:53:41 -0700

    changed title

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title> TrainerConnect | Change your Body</title>
+      <title>Normiss Fitness</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built with Next.js and TypeScript" />
       <script async src="//www.instagram.com/embed.js"></script>
